4202c802bdfe09e082df6cf930be95b5f7f1e9ab
Sharon <sharon@sharon-kasper.com>
2020-09-23 15:46:19 -0400

    Setting up user list component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <UserList msg="Welcome to Your Vue.js App"/>
+    <UserList />
   </div>
 </template>
 
---
+++ src/components/UserList.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="user-list">
+    <h1>List of Users</h1>
+    <ul v-for="user in users"  :user="user" :key="user.id">
+      <li>
+        
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'UserList',
+  props: {
+  },
+  data () {
+    return {
+      users: []
+    }
+  },
+  created () {
+    this.getUsers()
+  },
+  methods: {
+    getUsers () {
+      var self = this
+      const url = 'https://jsonplaceholder.typicode.com/users'
+      axios
+        .get(url)
+        .then(response => (self.users = response.data))
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
